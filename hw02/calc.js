d67a98406a02625b03b5e100e2fc9a340f407365
Jake Mittleman <jake@guest.guest>
2019-09-16 15:40:05 +0000

    fixing calculator functionality

--- hw02/calc.js
+++ hw02/calc.js
@@ -5,7 +5,8 @@
   "use strict";
 
   /* the current mathematical function */
-  var input1 = "0";
+  var expression = "0";
+  var currDisplay = "0";
 
   /* the object that will be updated to display on the calculator */
   var display = document.getElementById("display");
@@ -14,50 +15,145 @@
    * name: setInput
    * returns: nothing
    * parameter: num (a number string)
-   * does: if the input is blank set the input
+   * does: if the expression is blank set the expression
    *      otherwise concatenate the number.
    */
   function setInput(num) {
-    if (input1 === "0") {
-      input1 = num;
+    if (expression === "0") {
+      expression = num;
     } else {
-      input1 = input1.concat(num);
+      expression = expression.concat(num);
     }
   }
-
+  
 
   /*
    * name: isLastOperator
    * parameter: none
-   * returns: true if the last character in the input
+   * returns: true if the last character in the expression
    *          is an operator, false otherwise
    * does: checks if the last character is an operator
    */
-  function isLastOperator() { 
-    
+  function isLastOperator() {
     // checks if the last character is -, +, /, or *
-    if (input1.charAt(input1.length-1) === "-" ||
-        input1.charAt(input1.length-1) === "*" ||
-        input1.charAt(input1.length-1) === "/" ||
-        input1.charAt(input1.length-1) === "+") {
+    if (expression.charAt(expression.length-1) === "-" ||
+        expression.charAt(expression.length-1) === "*" ||
+        expression.charAt(expression.length-1) === "/" ||
+        expression.charAt(expression.length-1) === "+") {
   
       return true;
     }
 
     return false;
   }
+  
 
+  /*
+   * name: containsOperator
+   * parameters: none
+   * returns: true if the expression contains an operator
+   *          false otherwise
+   * does: iterates through the expression string and
+   *      searches for an operator (+, -, /, *)
+   */
+  function containsOperator() {
+    for (var i = 0; i < expression.length; i++) { 
+      if (expression.charAt(i) === "+" ||
+          expression.charAt(i) === "-" ||
+          expression.charAt(i) === "/" ||
+          expression.charAt(i) === "*") {
+    
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  /*
+   * name: evaluateExpression
+   * parameters: none
+   * returns: a number if the expression can be evaluated,
+   *          null otherwise
+   * does: tries to evaluate the expression and returns the
+   *      result if it can, otherwise returns null
+   */
+  function evaluateExpression() {
+    var result = null;
+    try {
+      result = eval(expression);
+      return result;
+    } catch (error) {
+      return result;
+    }
+  }
+  
   
   /*
    * name: changeDisplay
    * parameter: num (a number string)
    * returns: nothing
-   * does: sets the input string and updates the calculator
+   * does: sets the expression string and updates the calculator
    *      display with num added
    */
   function changeDisplay(num) {
-    setInput(num);
-    display.innerHTML = input1;
+    // if the calculator has just been opened
+    // replace the 0 with whatever number has been
+    // pressed
+    if (expression === "0") {
+      expression = num;
+      currDisplay = num;
+      display.innerHTML = num;
+    }
+
+    // if the expression ends with an operator
+    // concatenate the number and set the
+    // currDisplay to the number and update
+    // the calculator display
+    else if (isLastOperator()){
+      expression = expression.concat(num);
+      currDisplay = num;
+      display.innerHTML = currDisplay;
+    }
+  
+    // otherwise concatenate the number
+    // to the expression and the current display
+    // and update the calculator's display
+    else {
+      expression = expression.concat(num);
+      currDisplay = currDisplay.concat(num);
+      display.innerHTML = currDisplay;
+    }
+
+  }
+
+  /*
+   * name: tryToEvaluate
+   * parameters: none
+   * returns: true if possible to evaluate, false otherwise
+   *          (note: eval() works on a single number, but
+   *          this method considers a single number not
+   *          possible to evaluate)
+   * does: checks if there exists an operator in the expression
+   *      if there is, we try to evaluate it and if we can evaluate it
+   *      we update the expression and the currDisplay and display the
+   *      result.
+   */
+  function tryToEvaluate() {
+    
+    // if there's an operator
+    if (containsOperator()) {
+      
+      // try to evaluate
+      var result = evaluateExpression();
+      if (result != null) {
+        expression = "" + result;
+        currDisplay = "" + result;
+        display.innerHTML = result;
+        return true;
+      }
+    }
+    return false;
   }
   
 
@@ -70,75 +166,55 @@
    *      mathematical equation
    */
   function addEquals() {
-
-    /*
-     * Basically what happens here is we check if
-     * there's an operator (+, -, /, *) and if we
-     * find one, we try to evaluate. If for some reason
-     * it doesn't work (like "4 + 3 +") we just do nothing
-     * otherwise we evaluate the current equation
-     */
-    
-    // iterate through the input string
-    // and check if there's an operator
-    for (var i = 0; i < input1.length; i++) {
-      if (input1.charAt(i) === "+" ||
-          input1.charAt(i) === "-" ||
-          input1.charAt(i) === "/" ||
-          input1.charAt(i) === "*") {
-      
-
-
-          try {
-        
-            // try to evaluate the current input
-            // and update the display
-            var newVal = eval(input1);
-            input1 = "" + newVal;
-            display.innerHTML = input1;
-            return;
-
-            // otherwise do nothing
-          } catch (error) {
-            return;
-          }
-      }
+    var success = tryToEvaluate();
+    if (!success) {
+      expression = expression.concat("+");
     }
-
-    // if no operator was found add a + sign
-    // so if there's just a number.
-    changeDisplay("+");
   }
-
+  
 
   /*
    * name: sub
    * parameters: none
    * returns: nothing
    * does: checks if the last character
-   *      in the input string is a "-"
-   *      and if not, updates the input
+   *      in the expression string is a "-"
+   *      and if not, updates the expression
    *      and display
    */
   function sub() {
-    if (!isLastOperator()) {
-      changeDisplay("-");  
+    var success = tryToEvaluate();
+
+    if (!success) {
+      if (!isLastOperator()) {
+        expression = expression.concat("-");  
+      } 
+    }
+    else {
+      expression = expression.concat("-");
     }
   }
-
 
   /*
    * name: mul
    * parameters: none
    * returns: nothing
    * does: checks if the last character
-   *      in the input string is a "*"
-   *      and if not, updates the input
+   *      in the expression string is a "*"
+   *      and if not, updates the expression
    *      and display
    */
-  function mul() {
-    if (!isLastOperator()) {
-      changeDisplay("*");
+  function mul() { 
+    var success = tryToEvaluate();
+    
+    if (!success){
+      if (!isLastOperator()) {
+        expression = expression.concat("*");
+      }
+    }
+
+    else {
+      expression = expression.concat("*");
     }
   }
 
@@ -148,13 +224,21 @@
    * parameters: none
    * returns: nothing
    * does: checks if the last character
-   *      in the input string is a "/"
-   *      and if not, updates the input
+   *      in the expression string is a "/"
+   *      and if not, updates the expression
    *      and display
    */
   function div() {
-    if (!isLastOperator()) {
-      changeDisplay("/");
+    var success = tryToEvaluate();
+    
+    if (!success) {  
+      if (!isLastOperator()) {
+        expression = expression.concat("/");
+      }
+    }
+
+    else {
+      expression = expression.concat("/");
     }
   }
 
@@ -163,49 +247,22 @@
    * name: dec
    * parameters: none
    * returns: nothing
-   * does: finds if there's a decimal point
-   *      after the most recent operator. If
-   *      so, does nothing. Otherwise it adds
-   *      either "0." if the last character is
-   *      an operator (so that it's -0.x instead
-   *      of -.x) or "." if the last character
-   *      is a number.
+   * does: checks if the current number
+   *      on the display contains a decimal.
+   *      if it doesn't, it checks if the last
+   *      character in the expression is a number
+   *      and if not, it adds a 0 before the decimal.
+   *
+   *      This means that you will have x-0.y instead
+   *      of x-.y
    */
   function dec() {
-
-    // start the index at the last character
-    var index = input1.length-1;
-
-    // search backwards through the input string for
-    // an operator. If we find one, update index
-    // and break the search (we only need the most recent
-    // operator.)
-    for (var i = input1.length-1; i >=0; i--) {
-      if (input1.charAt(i) === "+" ||
-          input1.charAt(i) === "-" ||
-          input1.charAt(i) === "/" ||
-          input1.charAt(i) === "*") {
-    
-
-        index = i;
-        break;
+    if (!currDisplay.includes(".")) {
+      if (isLastOperator()) {
+        changeDisplay("0.");
+      } else {
+        changeDisplay(".");
       }
-      
-      // if we find a decimal before finding an operator
-      // then we can't add a new decimal point
-      else if (input1.charAt(i) === ".") {
-        return;
-      }
-
-    }
-
-    // if the last character is an operator (+. -. *, /)
-    // add a 0 before the decimal, otherwise just add
-    // the decimal
-    if (isLastOperator()) {
-      changeDisplay("0.");
-    } else {
-      changeDisplay(".");
     }
   }
 
@@ -214,11 +271,12 @@
    * name: clear
    * parameters: none
    * returns: nothing
-   * does: sets the input to 0 and updates the display.
+   * does: sets the expression to 0 and updates the display.
    */
   function clear() {
-    input1 = "0";
-    display.innerHTML = input1;
+    expression = "0";
+    currDisplay = "0";
+    display.innerHTML = expression;
   }
  
 
